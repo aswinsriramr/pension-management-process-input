dc02429a212346f606a83a6e2e9227f3e5714332
aswin2904 <sriramrengarajan99@gmail.com>
2022-05-30 18:21:29 +0530

    second

--- src/main/java/com/pension/processpensioninput/proxy/PensionerDetailsProxy.java
+++ src/main/java/com/pension/processpensioninput/proxy/PensionerDetailsProxy.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="pensioner-detail",url="http://localhost:9002/pensionerdetail",configuration = {FeignConfig.class})
+@FeignClient(name="pensioner-detail",url="http://35.78.226.167:8200/pensionerdetail",configuration = {FeignConfig.class})
 public interface PensionerDetailsProxy {
 	
 	@GetMapping("/{aadharNumber}")
